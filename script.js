b6b01cdfc1a3225d8e110502bac4148361bef0c3
razack00 <121952296+razack00@users.noreply.github.com>
2023-05-25 23:27:59 +0100

    fixed page switching

--- script.js
+++ script.js
@@ -6,18 +6,47 @@ function switchPage(newPage) {
 
     const currentPage = document.querySelector(newPage)
     currentPage.style.display = 'flex'
-    console.log(currentPage)
 }
 
 document.addEventListener('DOMContentLoaded', () => {
-    switchPage('.hero-section')
-    const sections = document.querySelectorAll('nav a')
-    sections.forEach(section => {  
-        section.onclick = (e) => {
-            page = `.${e.target.dataset.section}`
-            if(section !== page) {
+    switchPage('.about-section')
+    const links = document.querySelectorAll('nav a')
+    links.forEach(link => {  
+        link.onclick = (e) => {
+            page = `.${e.target.dataset.page}`
+            console.log(page)
+            if(link !== page) {
                 switchPage(page)
             }
         }
     })
+
+    const title = document.querySelector('.intro')
+    const text = "WebDeveloper &Designer".split(" ")
+    text.forEach(function(word) {
+        let chars = word.split('')
+        let div = document.createElement('div')
+        chars.forEach(char => {
+            let span = document.createElement('span')
+            span.textContent = char
+            div.appendChild(span)
+        })
+        title.appendChild(div)
+    })
+
+
+    const cursortext = document.querySelector('.cursor');
+    cursortext.innerHTML = cursortext.textContent.replace(/\S/g, "<span>$&</span>")
+
+    const element = document.querySelectorAll('.cursor span')
+    for(let i = 0; i<element.length; i++){
+        element[i].style.transform = "rotate("+i*12.2+"deg)"
+    }
+
+    let cursor = document.querySelector('.cursor')
+    document.addEventListener('mousemove', (e) => {
+        cursor.style.cssText =  'left: ' + (e.clientX+30) + 'px; top: ' + (e.clientY+40) + 'px;';
+    })
 })
+
+
